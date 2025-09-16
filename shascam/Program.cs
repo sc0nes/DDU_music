8e3b63ccb43b25928d4679303fe305363a6ce6ee
Sam-The-Man009 <117099086+Sam-The-Man009@users.noreply.github.com>
2025-09-16 13:01:16 +0200

    FFT fun

--- shascam/Program.cs
+++ shascam/Program.cs
@@ -4,9 +4,10 @@ using FFMpegCore.Arguments;
 using NAudio.Wave;
 using shascam.AudioProcessor;
 using System.IO;
-using shascam.Algorithm;
 using System;
-
+using MathNet.Numerics.IntegralTransforms;
+using NAudio.Dsp;
+using shascam.Algorithm;
 //Console.WriteLine("Hello, ");
 
 class Programio
@@ -19,89 +20,46 @@ class Programio
 
         // generelle kodestruktur her følger https://www.royvanrijn.com/blog/2010/06/creating-shazam-in-java/
         string filePath;
-        bool flowControl = FindCorrectPath(out filePath);
+        bool flowControl = shascam.FileHandler.FindCorrectPath(out filePath);
         if (!flowControl)
         {
             return;
         }
 
         //sampling
-        float[] samples = LoadWav(filePath);
+        float[] samples = shascam.FileHandler.LoadWav(filePath);
         //Array.ForEach(samples, Console.WriteLine);
 
         int windowSize = 2048;
 
-        for (int offset = 0; offset < samples.Length; offset += windowSize) // probably buggy
+        for (int offset = 0; offset < samples.Length - windowSize; offset += windowSize) // probably buggy
         {
             double[] window = new double[windowSize];
+            var complex = new System.Numerics.Complex[windowSize];
             for (int i = 0; i < windowSize; i++)
             {
                 window[i] = samples[offset + i];
+                complex[i] = new System.Numerics.Complex(window[i], 0);
             }
-            Array.ForEach(window, Console.WriteLine);
+            Fourier.Forward(complex, FourierOptions.Matlab);
+        
+            float[] ampl = new float[windowSize / 2]; // half become imaginary, we only want the reals
+            for (int i = 0; i < windowSize / 2; i++)
+            {
+                ampl[i] = (float)complex[i].Magnitude;
+            }
+            Array.ForEach(ampl, Console.WriteLine);
 
-            
-            float[] magnitudes; // = some fft stuff
-
-
+            Algorithm.shascam(ampl);
 
             //Algorithm.shascam(magnitudes);
 
 
 
-        }
+            }
 
     }
 
-    private static bool FindCorrectPath(out string filePath)
-    {
-        filePath = "test.wav";
-        if (!File.Exists(filePath))
-        {
-            filePath = "test.mp3";
-            if (!File.Exists(filePath)) return false;
-            var ap = new AudioProcessor();
-            string wavPath = filePath + ".wav";
-            ap.ConvertToWav(filePath, wavPath);
-            Console.WriteLine("File not found: " + filePath);
-        }
-        if (!File.Exists(filePath))
-        {
-            filePath = "test.wav";
-            Console.WriteLine("File not found: " + filePath);
-            return false;
-        }
-        Console.WriteLine($"File: {filePath}");
-        
-        return true;
-    }
-    static float[] LoadWav(string path)
-    {
-        using var reader = new AudioFileReader(path);
-        var samples = new float[reader.Length / sizeof(float)];
-        int read = reader.Read(samples, 0, samples.Length);
-        Array.Resize(ref samples, read);
-
-
-        WaveFormat format = GetFormat();
-        Console.WriteLine($"Sample rate: {reader.WaveFormat.SampleRate}, " +
-                            $"Channels: {reader.WaveFormat.Channels}, " +
-                            $"BitsPerSample: {reader.WaveFormat.BitsPerSample}");
-
-        return samples;
-    }
-
-    static WaveFormat GetFormat()
-    {
-        int sampleRate = 44100;
-        int sampleSizeInBits = 16;
-        int channels = 1;//tk increase this to 2, and make it work with stereo
-        return new WaveFormat(sampleRate, sampleSizeInBits, channels);
-    }
-
-    static void  DataAvailable(object sender, WaveInEventArgs e) // this function was taken from https://markheath.net/post/how-to-record-and-play-audio-at-same
-    {
-        recordedStream.Write(e.Buffer, 0, e.BytesRecorded);
-    }
+    
 
 }
--- shascam/obj/Debug/net9.0/shascam.AssemblyInfo.cs
+++ shascam/obj/Debug/net9.0/shascam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shascam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb02ab729e2a848a245cea2739e2905e74d8af75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2f7608be3b9eb38fc738b295b307ccf7a8af3cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("shascam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shascam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
