2be36840331b94d3699cb8c5564ea2edf5d71bb6
futte2626 <carl-emil@fambjornbak.dk>
2025-09-18 13:19:35 +0200

    Added a empty database

--- shascam/obj/Debug/net9.0/shascam.AssemblyInfo.cs
+++ shascam/obj/Debug/net9.0/shascam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shascam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e3b63ccb43b25928d4679303fe305363a6ce6ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e4f45c889e4dfcf88af4fab5a96ccab4ef6da81")]
 [assembly: System.Reflection.AssemblyProductAttribute("shascam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shascam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
