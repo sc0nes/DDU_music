757a8505f217a4fda9b410fa540840261321f976
Sam-The-Man009 <117099086+Sam-The-Man009@users.noreply.github.com>
2025-10-02 13:22:59 +0200

    this again

--- shascam/obj/Debug/net9.0/shascam.AssemblyInfo.cs
+++ shascam/obj/Debug/net9.0/shascam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shascam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+122e7d9cb8da858bf30b4f9d4a8353fa870f23fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11a340ad99b245db4c61dcf979011c8ec43b96ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("shascam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shascam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
